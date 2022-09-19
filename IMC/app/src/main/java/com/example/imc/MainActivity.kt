e11e1a48dd2ffc360aab71eb4f4833faba3060d7
IgorPaulinoDaSilva <103544198+IgorPaulinoDaSilva@users.noreply.github.com>
2022-09-18 21:09:26 -0300

    SharedPreferences
    Adicionado para salvar o último cálculo feito pelo usuário.

--- IMC/app/src/main/java/com/example/imc/MainActivity.kt
+++ IMC/app/src/main/java/com/example/imc/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.imc
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,9 +11,31 @@ import android.widget.TextView
 import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sharedPref: SharedPreferences;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sharedPref = getSharedPreferences("com.example.IMC.PERFIL", Context.MODE_PRIVATE) ?: return
+
+        val pesoAtualizado = recuperaPeso()
+        val alturaAtualizado = recuperaAltura()
+        val IMCAtualizado = recuperaIMC()
+        val situacaoAtualizado = recuperaSituacao()
+        if(pesoAtualizado != null && pesoAtualizado != "") {
+            findViewById<TextView>(R.id.peso).text = pesoAtualizado
+            findViewById<TextView>(R.id.altura).text = alturaAtualizado
+
+            val resultadoIMC = findViewById<TextView>(R.id.resultadoCal)
+            val resultadoSituacao = findViewById<TextView>(R.id.situacao)
+
+            resultadoIMC.text = IMCAtualizado
+            resultadoSituacao.text = situacaoAtualizado
+            resultadoIMC.visibility = View.VISIBLE
+            resultadoSituacao.visibility = View.VISIBLE
+        }
 
         val botao = findViewById<Button>(R.id.botao)
         botao.setOnClickListener {
@@ -49,10 +73,34 @@ class MainActivity : AppCompatActivity() {
                 situacao = "Obesidade grau III"
             }
 
+            with(sharedPref.edit()) {
+                putString("PESO", peso.text.toString())
+                putString("ALTURA", altura.text.toString())
+                putString("IMC", imc.toString())
+                putString("SITUACAO", situacao)
+                commit()
+            }
+
             resultadoIMC.setText(imc.toString())
             resultadoSituacao.setText(situacao)
             resultadoIMC.visibility = View.VISIBLE
             resultadoSituacao.visibility = View.VISIBLE
         }
+    }
+
+    fun recuperaPeso() : String? {
+        return sharedPref.getString("PESO", null)
+    }
+
+    fun recuperaAltura() : String? {
+        return sharedPref.getString("ALTURA", null)
+    }
+
+    fun recuperaIMC() : String? {
+        return sharedPref.getString("IMC", null)
+    }
+
+    fun recuperaSituacao() : String? {
+        return sharedPref.getString("SITUACAO", null)
     }
 }
